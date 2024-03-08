5ecaa2046f4f5d74d2928569645ca761e6c2cd6a
David Faitelson <davidf@afeka.ac.il>
2024-03-08 22:12:42 +0200

    mostly work on editing links and initial work on updating and merging

--- postgres/bootstrap.sql
+++ postgres/bootstrap.sql
@@ -1,7 +1,8 @@
 
-call new_changeset('bootstrap the object system', 
-'[{"oid":-1,"slots":{}, "description":"Class\n\nI am the initial object from which all other objects are ultimately cloned. I am the prototype of all top level class objects.", "links":[]}
-]', NULL);
+START TRANSACTION;
 
-INSERT INTO global (oid,name) VALUES(1, 'Class');
+call new_changeset('bootstrap the object system','[{"oid":-1,"slots":{}, "description":"", "links":[]}, {"oid":-2,"slots":{}, "description":"Class\n\nI am the initial object from which all other objects are ultimately cloned. I am the prototype of all top level class objects.", "links":[]}]', NULL);
 
+call new_changeset('add the class object to the global namespace','[{"oid":1,"slots":{}, "description":"This is the global namespace.", "links":[{"target":2, "name":"Class"}]}]', NULL);
+
+COMMIT;
